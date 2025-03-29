f5774888a9c3850ed453b76f238699e1d4c8f81a
Joelle <joelle.marianek@live.de>
2025-03-30 00:40:37 +0100

    feat: start working on layout, header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Button } from '@/components/ui/button';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header>
+          <Button>Sign In</Button>
+        </header>
+        <main>{children}</main>
+        <footer>Link | 2025 | Logo</footer>
       </body>
     </html>
   );
