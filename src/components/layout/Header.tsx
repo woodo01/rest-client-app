19a511693da372aec155a3f65be18834ad367102
Joelle <joelle.marianek@live.de>
2025-03-31 19:40:07 +0200

    feat: animate fixed header with color change

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -9,11 +9,31 @@ import {
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
 import Image from 'next/image';
+import clsx from 'clsx';
 
 const Header = () => {
   const [position, setPosition] = React.useState('top');
+  const [headercolor, setHeaderColor] = useState(false);
+
+  const changeColor = () => {
+    if (window.scrollY >= 56) {
+      setHeaderColor(true);
+    } else {
+      setHeaderColor(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeColor);
+  }, []);
+
   return (
-    <header className="flex justify-around p-2">
+    <header
+      className={clsx(
+        headercolor ? 'bg-muted-foreground' : 'bg-background',
+        'flex justify-around p-2 fixed top-0 right-0 left-0 transition-colors duration-300'
+      )}
+    >
       <Image src="/rest-api.svg" alt="Logo" width={40} height={40} />
       <DropdownMenu>
         <DropdownMenuTrigger className="p-1 border rounded-sm border-blue-950">
