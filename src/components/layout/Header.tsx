8d4559cfb9cc2d776ba6a2997d17338c6ae0649d
Joelle <joelle.marianek@live.de>
2025-03-31 14:50:43 +0200

    feat:basic layout of header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,37 @@
+'use client';
+import React from 'react';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
+import Image from 'next/image';
 
 const Header = () => {
+  const [position, setPosition] = React.useState('top');
   return (
-    <header className="flex justify-between">
+    <header className="flex justify-around p-2">
+      <Image src="/rest-api.svg" alt="Logo" width={40} height={40} />
       <DropdownMenu>
-        <DropdownMenuTrigger>Languages</DropdownMenuTrigger>
+        <DropdownMenuTrigger className="p-1 border rounded-sm border-blue-950">
+          Languages
+        </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuItem>English</DropdownMenuItem>
-          <DropdownMenuItem>Deutsch</DropdownMenuItem>
+          <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+            <DropdownMenuRadioItem value="top">English</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="bottom">
+              Deutsch
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
         </DropdownMenuContent>
       </DropdownMenu>
-      <Button>Sign In</Button>
-      <Button>Sign Up</Button>
+      <div className="flex gap-4">
+        <Button>Sign In</Button>
+        <Button>Sign Up</Button>
+      </div>
     </header>
   );
 };
