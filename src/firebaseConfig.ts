936f833abceafbb43a80ed08bfbd968b37a15315
Joelle <joelle.marianek@live.de>
2025-04-02 01:48:30 +0200

    feat: start setting up firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,25 @@
+// import { initializeApp } from "firebase/app";
+// ​​import {
+// ​​  GoogleAuthProvider,
+// ​​  getAuth,
+// ​​  signInWithPopup,
+// ​​  signInWithEmailAndPassword,
+// ​​  createUserWithEmailAndPassword,
+// ​​  sendPasswordResetEmail,
+// ​​  signOut,
+// ​​} from "firebase/auth";
+// ​​import {
+// ​​  getFirestore,
+// ​​  query,
+// ​​  getDocs,
+// ​​  collection,
+// ​​  where,
+// ​​  addDoc,
+// ​​} from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "process.env.NEXT_PUBLIC_FIREBASE_API_KEY",
+// }
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
