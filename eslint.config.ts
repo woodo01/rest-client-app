641b7d42b7ac749f7feb0f737c6f19070584a03a
Joelle <joelle.marianek@live.de>
2025-04-10 18:10:07 +0200

    feat: start working on convertign all js files to ts

---
+++ eslint.config.ts
@@ -0,0 +1,33 @@
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import typescriptPlugin from '@typescript-eslint/eslint-plugin'
+// import * as typescriptEslint from '@typescript-eslint/eslint-plugin';
+// import type { Linter } from 'eslint';
+
+// import  { defineConfig } from "eslint/config";
+
+// export default defineConfig([
+//   {
+//     files: [
+//       '**/*.ts',
+//       '**/*.tsx',
+//     ],
+//     rules: {
+//       '@typescript-eslint/explicit-function-return-type': 'error',
+//     },
+//     ignores: [
+//       '**/node_modules/**',
+//       '**/dist/**',
+//       '**/coverage/**',
+//       '**/ui/**',
+//     ],
+//     extends: [
+//       'next/core-web-vitals',
+//       'next/typescript',
+//       'prettier',
+//     ],
+//     plugins: {
+//       '@typescript-eslint': typescriptPlugin as Linter.Plugin,
+//     }
+//   }
+// ])
