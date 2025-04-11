51439d655e833ddfeb05d1f18544b03f21e13ddf
Joelle <joelle.marianek@live.de>
2025-04-11 16:05:07 +0200

    feat: change eslintconfig to ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,33 +1,25 @@
-// import { dirname } from 'path';
-// import { fileURLToPath } from 'url';
-// import typescriptPlugin from '@typescript-eslint/eslint-plugin'
-// import * as typescriptEslint from '@typescript-eslint/eslint-plugin';
-// import type { Linter } from 'eslint';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-// import  { defineConfig } from "eslint/config";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// export default defineConfig([
-//   {
-//     files: [
-//       '**/*.ts',
-//       '**/*.tsx',
-//     ],
-//     rules: {
-//       '@typescript-eslint/explicit-function-return-type': 'error',
-//     },
-//     ignores: [
-//       '**/node_modules/**',
-//       '**/dist/**',
-//       '**/coverage/**',
-//       '**/ui/**',
-//     ],
-//     extends: [
-//       'next/core-web-vitals',
-//       'next/typescript',
-//       'prettier',
-//     ],
-//     plugins: {
-//       '@typescript-eslint': typescriptPlugin as Linter.Plugin,
-//     }
-//   }
-// ])
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+    ignores: ['**/node_modules/**', '**/dist/**', '**/coverage/**', '**/ui/**'],
+  }
+);
