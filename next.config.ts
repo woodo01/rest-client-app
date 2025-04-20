0353f415c14b1af90c93eb47be3e7b5edb694ea9
Ivan Sigaev <ivan.develop@gmail.com>
2025-04-20 18:16:59 +0200

    feat: add basic intl logic

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+import { NextConfig } from "next/dist/server/config-shared";
+
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
